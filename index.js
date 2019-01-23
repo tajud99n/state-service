95baa3f25fb1a6968de3130917b1bee907410b5f
tajud99n <giwa208@gmail.com>
2019-01-23 16:55:17 +0100

    addition of storage library

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const url = require('url');
 const stringDecoder = require('string_decoder').StringDecoder;
 const config = require('./config');
 const fs = require('fs');
+const _data = require('./lib/data');
+
 
 // Instantiate the HTTP server
 const httpServer = http.createServer(function (req, res) {
