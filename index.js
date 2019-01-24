8217215b6ec696a3326b4da9d02a26512b20664c
tajud99n <giwa208@gmail.com>
2019-01-24 20:09:38 +0100

    creation of users endpoints

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const http = require('http');
 const https = require('https');
 const url = require('url');
 const stringDecoder = require('string_decoder').StringDecoder;
-const config = require('./config');
+const config = require('./lib/config');
 const fs = require('fs');
 const _data = require('./lib/data');
+const handlers = require('./lib/handlers');
+const helpers = require('./lib/helpers');
 
 
 // Instantiate the HTTP server
@@ -75,7 +77,7 @@ let unifiedServer = function (req, res) {
              'queryStringObject': queryStringObject,
              'method': method,
              'headers': headers,
-             'payload': buffer
+             'payload': helpers.parseJsonToObject(buffer)
          };
 
          // Route the request to the handler specified in the router
@@ -102,20 +104,10 @@ let unifiedServer = function (req, res) {
      });
 };
 
-// Define the handlers
-let handlers = {};
 
-// Ping handler
-handlers.ping = function (data, callback) {
-    callback(200);
-};
-
-// Not found handler
-handlers.notFound = function (data, callback) {
-    callback(404);
-};
 
 // Define a request router
 const router = {
-    'ping': handlers.ping
+    'ping': handlers.ping,
+    'users': handlers.users
 }
